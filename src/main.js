c0059e733ed37307ef4f8ebbe296627633bfd27f
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-05-07 00:22:19 +0700

    removed old buefy style

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Buefy from 'buefy' 
-import 'buefy/dist/buefy.css' 
 import VueLodash from 'vue-lodash'
 import lodash from 'lodash'
 
 Vue.use(Buefy)
-
 Vue.use(VueLodash, { lodash: lodash })
 
 Vue.config.productionTip = false
