87df2127c3491885ad8aea475b02c49853a2a214
Jamie Sathira <nong.jamie.jamie@gmail.com>
2020-05-07 00:25:23 +0700

    changed style of the header and added the credit below the date paragraph

--- src/views/header/Header.vue
+++ src/views/header/Header.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="bg-style">
-    <div class='flex flex-col items-center justify-center' style='height: 100%;'>
+  <div class="bg-style h-screen">
+    <div class='flex flex-col items-center justify-center h-full'>
       <p class='text-4xl sm:text-5xl md:text-6xl text-white text-center'>
         COVID19 CRISIS SUMMARY
       </p>
       <p class='text-3xl sm:text-4xl md:text-5xl text-white text-center uppercase'>
-        {{ setDateFormat(summaryDateProps, "DD MMMM YYYY") }}
+        {{ summaryDateProps ? setDateFormat(summaryDateProps, "DD MMMM YYYY") : '' }}
+      </p>
+      <p class='text-xs sm:text-sm md:text-base text-white text-center uppercase mt-4 cursor-pointer' @click='creditClicked()'>
+        Created by Sathira K.
       </p>
     </div>
   </div>
@@ -17,7 +20,13 @@ import dateMixin from '@/mixins/date.js';
 export default {
   mixins: [ dateMixin ],
   props: {
-    summaryDateProps: String
+    summaryDateProps: String,
+    githubLink: 'https://github.com/nongjamie'
+  },
+  methods: {
+    creditClicked() {
+      window.location.href = this.githubLink;
+    }
   }
 }
 </script>
@@ -25,7 +34,6 @@ export default {
 <style scoped>
 .bg-style {
   width: 100%;
-  height: 500px;
   background-size: cover;
   background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), 
   url("https://cdn3.eyeem.com/thumb/4d63967484f539e700208e60ad71ca5ebafabd7f-1527513585138/w/850");
